be2ec9c26ff84cff276f847aaacd9f71b6f77b51
Joseph Hendrix <JLH7459@wireless-10-145-45-214.public.utexas.edu>
2024-11-18 09:47:05 -0600

    .gitignore additions, formatting/linter changes

--- pubscraper/APIClasses/PubMed.py
+++ pubscraper/APIClasses/PubMed.py
@@ -3,14 +3,16 @@ import json
 import time
 import logging
 
-format_str=f'[%(asctime)s ] %(filename)s:%(funcName)s:%(lineno)s - %(levelname)s: %(message)s'
-logging.basicConfig(level=logging.WARNING, format=format_str)
+format_str = (
+    f"[%(asctime)s ] %(filename)s:%(funcName)s:%(lineno)s - %(levelname)s: %(message)s"
+)
+logging.basicConfig(level=logging.DEBUG, format=format_str)
 
 
 class PubMed:
     def __init__(self):
-        self.search_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
-        self.summary_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi'
+        self.search_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+        self.summary_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
 
     def get_UIDs_by_author(self, author_name, rows=10):
         """
@@ -32,31 +34,33 @@ class PubMed:
         else:
             entrez_author_name = split_name[-1] + "+"
             for name in split_name[:-1]:
-                entrez_author_name += "{initial}".format(initial = name[0])
+                entrez_author_name += "{initial}".format(initial=name[0])
             # entrez_author_name = author_name.replace(" ", "+")
             entrez_author_name += "[Author Name]"
-        logging.info(f'searching for publications by {entrez_author_name}')
+        logging.info(f"searching for publications by {entrez_author_name}")
 
         params = {
-            'db':'pmc',
-            'term': entrez_author_name,
-            'retmax': rows,
-            'retmode': 'JSON',
+            "db": "pmc",
+            "term": entrez_author_name,
+            "retmax": rows,
+            "retmode": "JSON",
         }
 
         response = requests.get(self.search_url, params=params)
 
         if response.status_code != 200:
-            logging.error(f'Error fetching data from PubMed: {response.status_code}')
+            logging.error(f"Error fetching data from PubMed: {response.status_code}")
             # print(f"Error fetching data from PubMed: {response.status_code}")
             return None
 
         data = response.json()
         logging.debug(json.dumps(data, indent=2))
-        id_list = data['esearchresult']['idlist']
+        id_list = data["esearchresult"]["idlist"]
 
         if not id_list:
-            logging.warning(f"Received no UIDs for author {author_name}, returning None")
+            logging.warning(
+                f"Received no UIDs for author {author_name}, returning None"
+            )
             logging.debug(f'passed "{entrez_author_name}" to PubMed API')
             return None
 
@@ -72,28 +76,30 @@ class PubMed:
         if not UIDs:
             logging.warning("received no UIDs, returning None")
             return None
-        
+
         # join UIDs list into one 'csv' string
-        stringified_UIDs = ','.join(UIDs)
+        stringified_UIDs = ",".join(UIDs)
         params = {
-            'db': 'pmc',
-            'id': stringified_UIDs,
-            'retmode': 'JSON',
+            "db": "pmc",
+            "id": stringified_UIDs,
+            "retmode": "JSON",
         }
 
         response = requests.get(self.summary_url, params=params)
         if response.status_code != 200:
-            logging.error(f"Error fetching summaries from PubMed: {response.status_code}")
+            logging.error(
+                f"Error fetching summaries from PubMed: {response.status_code}"
+            )
             return
 
         data = response.json()
 
         publications = []
-        for uid in data['result']['uids']:
-            summary_object = data['result'][uid]
+        for uid in data["result"]["uids"]:
+            summary_object = data["result"][uid]
             author_list = []
-            for author_object in summary_object['authors']:
-                author_list.append(author_object['name'])
+            for author_object in summary_object["authors"]:
+                author_list.append(author_object["name"])
 
             # pub = Publication(
             #     journal=summary_object['fulljournalname'],
@@ -102,16 +108,15 @@ class PubMed:
             #     authors=author_list
             # )
             pub = {
-                'id': uid,
-                'journal': summary_object['fulljournalname'],
-                'publication_date': summary_object['sortdate'],
-                'title': summary_object['title'],
-                'authors': ",".join(author_list)
+                "id": uid,
+                "journal": summary_object["fulljournalname"],
+                "publication_date": summary_object["sortdate"],
+                "title": summary_object["title"],
+                "authors": ",".join(author_list),
             }
             publications.append(pub)
 
         return publications
-
 
     def get_publications_by_author(self, author_name, rows=10):
         UIDs = self.get_UIDs_by_author(author_name, rows)
@@ -136,9 +141,10 @@ def search_multiple_authors(authors, rows=10):
             all_results[author] = publications
         except Exception as e:
             logging.error(f"Error fetching data for {author}: {e}")
-        time.sleep(0.4)    # avoids RESPONSE 429 (rate limit violation)
+        time.sleep(0.4)  # avoids RESPONSE 429 (rate limit violation)
 
     return all_results
+
 
 def main():
     # authors = ['albert', 'albert einstein', 'albert einsten']
@@ -150,5 +156,5 @@ def main():
     print(json.dumps(results, indent=2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_PubMed.py
+++ tests/test_PubMed.py
@@ -1,13 +1,16 @@
 import pytest
 from pubscraper.APIClasses import PubMed
 
+
 def test_skip_empty_name():
     results = PubMed.search_multiple_authors([""])
     assert results == {}
 
+
 def test_no_input():
     results = PubMed.search_multiple_authors([])
     assert results == {}
+
 
 def test_partial_empty_input():
     results = PubMed.search_multiple_authors(["albert", ""])
@@ -21,35 +24,39 @@ def test_search_all_names():
 
 def test_no_results():
     results = PubMed.search_multiple_authors(["joseph l hendrix"])
-    assert results == {'joseph l hendrix': None}
+    assert results == {"joseph l hendrix": None}
+
 
 def test_initials_lastname():
-    results = PubMed.search_multiple_authors(['w j allen'])
-    publications_found = results['w j allen']
+    results = PubMed.search_multiple_authors(["w j allen"])
+    publications_found = results["w j allen"]
     # iterate through publications_found to find a paper authored by Joe Allen
     biasnet_found = False
     for pub in publications_found:
-        if "BiasNet" in pub['title']:
+        if "BiasNet" in pub["title"]:
             biasnet_found = True
-    assert biasnet_found == True
+    assert biasnet_found
+
 
 def test_fullname():
-    results = PubMed.search_multiple_authors(['william joseph allen'])
-    publications_found = results['william joseph allen']
+    results = PubMed.search_multiple_authors(["william joseph allen"])
+    publications_found = results["william joseph allen"]
     # iterate thorugh publications_found to find a paper authored by Joe Allen
     biasnet_found = False
     for pub in publications_found:
-        if "BiasNet" in pub['title']:
+        if "BiasNet" in pub["title"]:
             biasnet_found = True
-    assert biasnet_found == True
+    assert biasnet_found
+
 
 def test_result_parity():
-    results_one = PubMed.search_multiple_authors(['william joseph allen'])
-    results_two = PubMed.search_multiple_authors(['w j allen'])
-    pubs_found_one = results_one['william joseph allen']
-    pubs_found_two = results_two['w j allen']
+    results_one = PubMed.search_multiple_authors(["william joseph allen"])
+    results_two = PubMed.search_multiple_authors(["w j allen"])
+    pubs_found_one = results_one["william joseph allen"]
+    pubs_found_two = results_two["w j allen"]
     assert pubs_found_one == pubs_found_two
 
+
 def test_limit_number_of_results():
-    results = PubMed.search_multiple_authors(['w j allen'], 2)
-    assert len(results['w j allen']) == 2
+    results = PubMed.search_multiple_authors(["w j allen"], 2)
+    assert len(results["w j allen"]) == 2
