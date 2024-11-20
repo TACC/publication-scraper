18515ec78c846fc1e580a8f5f35ef8afa306c04e
Joseph Hendrix <joeylhendrix@gmail.com>
2024-11-19 18:01:07 -0600

    crossref data extraction (WIP)

--- pubscraper/APIClasses/CrossRef.py
+++ pubscraper/APIClasses/CrossRef.py
@@ -12,6 +12,22 @@ class CrossRef:
     def __init__(self):
         self.base_url = "http://api.crossref.org/works"
 
+    def extract_authors(self, publication_item):
+        authors = []
+        for author in publication_item["author"]:
+            name = author["given"] + " " + author["family"]
+            authors.append(name)
+            logging.debug(f"added {name} to author list")
+        return authors
+
+    def extract_publication_date(self, publication_item):
+        logging.debug(json.dumps(publication_item, indent=2))
+        try:
+            date_list = publication_item["published-print"]["date-parts"]
+            return date_list
+        except KeyError:  # query does NOT contain publication date
+            return None
+
     def get_publications_by_author(self, author_name, rows=10):
         """
         Given the name of an author, search CrossRef for works written by
@@ -32,6 +48,7 @@ class CrossRef:
         params = {
             "query.author": author_name.replace(" ", "+"),
             "rows": "10",
+            "select": "author,title,container-title,published-print",
         }
 
         response = requests.get(self.base_url, params=params)
@@ -42,3 +59,35 @@ class CrossRef:
 
         data = response.json()
         logging.debug(json.dumps(data, indent=2))
+
+        publications = []
+
+        for publication_item in data["message"]["items"]:
+            logging.debug(publication_item["title"])
+            pub = {
+                "journal": publication_item["container-title"],
+                "publication_date": self.extract_publication_date(publication_item),
+                "title": publication_item["title"][0],
+                "authors": self.extract_authors(publication_item),
+            }
+            publications.append(pub)
+
+        return publications
+
+
+def search_multiple_authors(authors, rows=10):
+    pass
+
+
+def main() -> None:
+    author_names = input("Enter author names(comma-separated): ").split(".")
+    author_names = [name.strip() for name in author_names]
+
+    # result = search_multiple_authors(author_names)
+    # print(json.dumps(result, indent=2))
+    crossref = CrossRef()
+    crossref.get_publications_by_author("richard feynman")
+
+
+if __name__ == "__main__":
+    main()
