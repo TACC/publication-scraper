7ebcda267600d8230e48c67b1806edb7db9f4135
Joseph Hendrix <joeylhendrix@gmail.com>
2024-11-26 14:19:27 -0600

    main.py can now accept command line arguments (WIP)

--- pubscraper/main.py
+++ pubscraper/main.py
@@ -1,11 +1,73 @@
 import json
+import logging
+
+import click
+from click_loglevel import LogLevel
+
+from version import __version__
+import config
 from APIClasses.PubMed import PubMed
 from APIClasses.arXiv import ArxivAPI
 
+LOG_FORMAT = config.LOGGER_FORMAT_STRING
+LOG_LEVEL = config.LOGGER_LEVEL
+logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
+logger = logging.getLogger(__name__)
 
-def main():
+
+def set_logging_level(ctx, param, value):
+    """
+    Callback function for click that sets the logging level
+    """
+    logger.setLevel(value)
+    return value
+
+
+def set_log_file(ctx, param, value):
+    """
+    Callback function for click that sets a log file
+    """
+    if value:
+        fileHandler = logging.FileHandler(value, mode="w")
+        logFormatter = logging.Formatter(LOG_FORMAT)
+        fileHandler.setFormatter(logFormatter)
+        logger.addHandler(fileHandler)
+    return value
+
+
+@click.command()
+@click.version_option(__version__)
+@click.option(
+    "--log-level",
+    type=LogLevel(),
+    default=logging.INFO,
+    is_eager=True,
+    callback=set_logging_level,
+    help="Set the log level",
+    show_default=True,
+)
+@click.option(
+    "--log-file",
+    type=click.Path(writable=True),
+    is_eager=True,
+    callback=set_log_file,
+    help="Set the log file",
+)
+@click.option(
+    "-n",
+    "--number",
+    type=int,
+    default=10,
+    help="Specify max number of publications to receive for each author",
+)
+def main(log_level, log_file, number):
+    logger.debug(f"Logging is set to level {logging.getLevelName(log_level)}")
+    if log_file:
+        logger.debug(f"Writing logs to {log_file}")
     author_names = input("Enter author names(comma-separated): ").split(",")
     author_names = [name.strip() for name in author_names]
+
+    logger.debug(f"Requesting {number} publications for each author")
 
     apis = [PubMed(), ArxivAPI()]
     authors_and_pubs = []
@@ -15,7 +77,7 @@ def main():
         # FIXME: these names are too similar and confusing
         authors_pubs = []
         for api in apis:
-            pubs_found = api.get_publications_by_author(author)
+            pubs_found = api.get_publications_by_author(author, number)
             if pubs_found is not None:
                 authors_pubs += pubs_found
 
