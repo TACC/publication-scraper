9c8aee5023a152af9685df774b341004a4b8ecdd
Joseph Hendrix <joeylhendrix@gmail.com>
2024-12-09 17:22:04 -0600

    added run.sh script (WIP)

---
+++ run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRAPER_COMMAND=""
+IMAGENAME="joeleehen/pubscraper"
+IMAGEVERSION=${IMAGEVERSION:-latest}
+BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+FORCE_DOCKER=${FORCE_DOCKER:-0}
+
+die() {
+  echo "$!" 1>&2
+  exit 1
+}
+
+realpath() {
+  echo "$(
+    cd "$(dirname "$1")"
+    pwd
+  )"
+}
+
+# should we run installed version or from docker image?
+# check for installed version
+DOCKER_RUN=""
+# check for docker install
+docker info >/dev/null || die "Docker is not installed. Exiting"
+
+# check for image loaded
+DOCKER_IMAGE=$(docker images -q "${IMAGENAME}:${IMAGEVERSION}")
+
+# if not, build it
+if [ -z "${DOCKER_IMAGE}" ]; then
+  docker build -t "${IMAGENAME}:${IMAGEVERSION}" "${BUILD_DIR}"
+fi
+
+DOCKER_RUN="docker run -t --rm -v $(pwd):/publication-scraper/data ${IMAGENAME}:${IMAGEVERSION} "
+
+echo "${DOCKER_RUN}${SCRAPER_COMMAND} $@" 1>&2
+${DOCKER_RUN}${SCRAPER_COMMAND} "$@"
